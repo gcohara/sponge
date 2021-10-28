e69f95e076d6b7a13296a195ea2840b2485ae4c5
George O'Hara <gcoh@pm.me>
2021-10-28 08:27:59 -0700

    integer wrapping implementation completed

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -1,4 +1,8 @@
 #include "wrapping_integers.hh"
+#include <bits/stdint-uintn.h>
+#include <cstdint>
+#include <iostream>
+#include <limits>
 
 // Dummy implementation of a 32-bit wrapping integer
 
@@ -14,8 +18,9 @@ using namespace std;
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+    uint64_t seq_64 {n + isn.raw_value()};
+    WrappingInt32 out (seq_64 & UINT32_MAX);
+    return out;
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -29,6 +34,14 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    // The lower 32 bits of the output are simple to get.
+    auto out_lower_32 { n.raw_value() - isn.raw_value() };
+    auto out_candidate_higher_32 { checkpoint & (UINT64_MAX - UINT32_MAX) };
+    auto out_candidate { out_candidate_higher_32 + out_lower_32 };
+    if (out_candidate > checkpoint && ((out_candidate - checkpoint) > (UINT32_MAX / 2)) && (out_candidate_higher_32 != 0)) {
+        out_candidate -= (1ul << 32);
+    } else if (out_candidate < checkpoint && ((checkpoint - out_candidate) > (UINT32_MAX / 2)) && (out_candidate_higher_32 != (UINT64_MAX - UINT32_MAX))) {
+        out_candidate += (1ul << 32);
+    }
+    return out_candidate;
 }
